9bf20f1996aa754b4664d15105d3a4fdbc0d4b47
devdopz <teamdevdopz@gmail.com>
2024-04-16 20:53:15 +0530

    initial commit

---
+++ src/Components/Latest.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import styles from './Latest.module.css';
+
+const Latest = () => {
+  return (
+    <div className={styles.LatestHome}>
+      
+    </div>
+  )
+}
+
+export default Latest
